1111c6a7a7b8e1859a777a87cb191cfb8776adc9
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-02-12 19:02:28 +0400

    sql tutorials

--- sql_op1.sql
+++ sql_op1.sql
@@ -35,4 +35,61 @@ where user_Date='30.09.1998';
 alter table test 
 add column created_at timestamp default current_timestamp;
 
+-- The AND operator displays a record if all the conditions separated by AND are TRUE.
+select * from test
+where user_Name='Raheddin' and user_id=4;
+
+-- The OR operator displays a record if any of the conditions separated by OR is TRUE.
+select * from test
+where user_ID=4 or user_Date='08.08.1998';
+
+-- The NOT operator displays a record if the condition(s) is NOT TRUE.
+select * from test
+where not user_ID=1;
+
+-- ORDER BY  ASC-ascending (ARTAN), DESC-Ddescending(AZALAN):
+SELECT user_Name from test
+order by user_ID DESC;
+
+-- Update:
+update test
+set user_Name='Taceddin', user_Date='15.12.2003'
+where user_Id=1;
+
+-- Delete:
+delete from test where user_ID=6;
+
+-- LIMIT:
+SELECT * FROM test LIMIT 3;
+SELECT * FROM test WHERE user_Surname='Asadzade' LIMIT 2; 
+
+-- add new column
+alter table test 
+add column user_Age int(11) not null ;
+
+-- update
+update test 
+set user_age=24 where user_ID=5; 
+
+-- MIN AND MAX:
+select min(user_Age) AS min_Age from test; 
+select max(user_Age) as max_Age from test;
+
+-- COUNT(), AVG() and SUM() Functions:
+select count(user_Age) as Count from test;
+select avg(user_Age) as avg_Age from test;
+select sum(user_Age) as sum_Age from test;
+
+-- LIKE:
+SELECT * FROM test WHERE user_Surname
+like 'aa%';
+
+-- in :
+select * from test where user_Surname='Asadzade'
+in(user_ID=1 or user_ID=2 or user_ID=3);
+
+-- between:
+select * from test where user_ID
+between 1 and 3;
+
 -- 
