7b64854a08d8bef887216c9768e0cacc8ded567b
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-02-13 18:31:30 +0400

    sql tutorial

--- sql_op2.sql
+++ sql_op2.sql
@@ -1,3 +1,35 @@
-ALTER TABLE test
-MODIFY COLUMN user_ID int(11) NOT NULL AUTO_INCREMENT,
-ADD PRIMARY KEY (user_ID); 
+-- create new table 
+create table test_2(
+city_id INT AUTO_INCREMENT PRIMARY KEY,
+ city_name varchar(255) NOT NULL,
+ city_population int(11) not null);
+ 
+ -- select
+ select*from test_2;
+ 
+ -- add
+insert into test_2(city_name,city_population)
+values('Baku',2000000),
+('Lankaran', 800000),('Ganja',500000),
+('Qabala',300000);
+
+-- Full Joins
+select user_Name,t2.city_name 
+from test  join test_2 t2  on test.user_ID=t2.user_ID ;
+
+-- left join
+select *
+from test left join test_2
+on test.user_ID=test_2.user_ID;
+
+-- where compare with  joins
+select * from test , test_2
+where test.user_ID=test_2.user_ID;
+
+-- Inner Join:
+select t.user_Age,t.user_Name,t2.city_name,t2.city_population FROM test t inner join test_2 t2
+on t.user_ID=t2.user_ID; 
+
+-- right join:
+
+
