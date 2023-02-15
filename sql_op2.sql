b97e052a6b30cebb070df5e9e046292c6b76301d
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-02-15 18:08:42 +0400

    sql tutorial

--- sql_op2.sql
+++ sql_op2.sql
@@ -14,10 +14,14 @@ values('Baku',2000000),
 ('Qabala',300000);
 
 -- Full Joins
-select user_Name,t2.city_name 
-from test  join test_2 t2  on test.user_ID=t2.user_ID ;
+select *
+from test full join test_2 on test.user_ID=test_2.user_ID
+order by test.user_ID;
 
 -- left join
+-- left join olanda 2 table-in sol terefine baxaraq table-i
+-- yaradir yeni sol terefde  siralama 4 sagda 5 dirse ozaman tableleri 
+-- birlesdiren zaman yalniz 4-e qeder olanlari birlesdirir .
 select *
 from test left join test_2
 on test.user_ID=test_2.user_ID;
@@ -31,5 +35,32 @@ select t.user_Age,t.user_Name,t2.city_name,t2.city_population FROM test t inner 
 on t.user_ID=t2.user_ID; 
 
 -- right join:
+select *
+from test t right join test_2 t2 
+on t.user_ID=t2.user_ID;
+
+--  join*
+select t.user_Name,t2.city_name from test t ,test_2 t2
+where t.user_ID<>t2.user_ID
+and t.user_ID=t2.city_id order by t.user_Age asc;
+
+-- self join
+select * from test t, test t2 
+where t.user_ID<>t2.user_ID
+and t.user_Name=t.user_Name order by t.user_Date;
+
+-- union and union all:
+-- aralarindaki tek ferq union eyni olan stunu 2-ci defe vermir; 
+select user_Name from test
+union 
+select city_name from test_2;
+
+-- Group by:
+select SUM(user_ID),user_Name,user_Age from test
+group by user_Name ;
+
+
+
+
 
 
